e2bf01b255665015fd24320a22a26fdf5e380622
HasheebKazi <hasheeb.k@gmail.com>
2019-07-24 15:41:17 -0400

    first routes working with dummy data

--- bankend_server/app.js
+++ bankend_server/app.js
@@ -10,9 +10,9 @@ app.use(bodyParser.json());
 
 // avoid cross origin resource sharing error
 app.use((req, res, next) => {
-    res.sestHeader('Access-Control-Allow-Origin', '*');
-    res.sestHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.sestHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
 
--- bankend_server/controllers/controller_feed.js
+++ bankend_server/controllers/controller_feed.js
@@ -1,18 +1,46 @@
+const { validationResult } = require('express-validator/check');
+
 exports.getPosts = (req, res, next) => {
-    res.status(200).json({posts: [{title: 'First Post', content: 'This is the first post!'}]
+    res.status(200).json({
+        posts: [{
+            _id: '2',
+            title: 'First Post',
+            content: 'This is the first post!',
+            imageUrl: 'images/logo_2.jpg',
+            creator: {
+                name: 'Dogger'
+            }, 
+            createdAt: new Date()
+        }]
     });
 };
 
 exports.createPost = (req, res, next) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        res.status(422).json({
+            message: 'validation failed',
+            errors: errors.array()
+        })
+    }
+
+
     const title = req.body.title;
+    // console.log('title:', title);
+    console.log(req.body);
+
     const content = req.body.content
     // create post in databse;
     res.status(201).json({
         message: 'post created successfully',
-        post:  {
-            id: new Date().toISOString(),
+        post: {
+            _id: new Date().toISOString(),
             title: title,
-            content: content
+            content: content,
+            creator: {
+                name: 'pooper'
+            }, 
+            createdAt: new Date()
         }
     })
 };
